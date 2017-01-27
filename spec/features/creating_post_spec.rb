9ede9def6eac26a833a91b0790b25af0d75c69b0
Tjitske Lohstroh <tjitske@lohstroh.nl>
2017-01-27 14:35:39 +0100

    feature test creating post

---
+++ spec/features/creating_post_spec.rb
@@ -0,0 +1,28 @@
+# https://github.com/teamcapybara/capybara
+
+require "rails_helper"
+
+# test doesn't fully work yet because of authentication issues..
+# TODO make it work
+
+RSpec.feature "Creating Articles" do
+  # (scenario = capybara)
+  scenario "A user creates a new article" do
+    # visit root page
+    visit "/articles"
+    # click on new Article
+    click_on 'New'
+    # fill in title
+    fill_in "Title", with: "Creating a blog"
+    # fill in body
+    fill_in "Body", with: "Lorem Ipsum"
+    # create Article
+    click_button "Create"
+
+    #### expextations ###
+    # Article has been created
+    expect(page).to have_content("Article has been created")
+    # articles path
+    expect(page.current_path).to eq(articles_path)
+  end
+end
